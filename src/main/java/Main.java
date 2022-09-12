1a2ae9d5bbbe60447b158b872eb78dc81f34bcd7
«Kwork» <kwork@yandex.ru>
2022-09-12 17:25:07 +0200

    add code HW 6_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,72 +1,61 @@
 /**
- * Реализовать алгоритм перевода из инфиксной записи в постфиксную для арифметического выражения.
- * Вычислить запись если это возможно
+ * 1396. Design Underground System
+ * An underground railway system is keeping track of customer travel times between different stations.
+ * They are using this data to calculate the average time it takes to travel from one station to another.
  */
 
-import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         boolean isFormatted = false;
         String request = "пожалуйста форматируйте код";
-        // исправили теперь build succcessful
-//        if (isFormatted) {
-        String infix = "A+(B*C)-(D*E)+F/D";
-        String postfix = infixToPostfix(infix);
-        System.out.println(postfix);
-//        }
+//        System.out.println(getAverageTime(String AA, String BB));
     }
 
-    public static int prec(char c) {
-        if (c == '*' || c == '/') {
-            return 3;
+    class CheckInInfo {
+        private String stationName;
+        private int t;
+
+        CheckInInfo(String stationName, int t) {
+            this.stationName = stationName;
+            this.t = t;
         }
-        if (c == '+' || c == '-') {
-            return 4;
-        }
-        if (c == '&') {
-            return 8;
-        }
-        if (c == '^') {
-            return 9;
-        }
-        if (c == '|') {
-            return 10;
-        }
-        return Integer.MAX_VALUE;
     }
 
-    public static boolean isOperand(char c) {
-        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+    class CheckOutInfo {
+        private int total;
+        private int count;
+
+        CheckOutInfo(int total, int count) {
+            this.total = total;
+            this.count = count;
+        }
     }
 
-    public static String infixToPostfix(String infix) {
-        if (infix == null || infix.length() == 0) {
-            return infix;
+    private final Map<Integer, CheckInInfo> checkInMap = new HashMap<>();
+    private final Map<String, CheckOutInfo> checkOutMap = new HashMap<>();
+
+    public void checkIn(int id, String stationName, int t) {
+        checkInMap.put(id, new CheckInInfo(stationName, t));
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+        CheckInInfo checkInInfo = checkInMap.get(id);
+        String checkOutKey = String.format("%s-%s", checkInInfo.stationName, stationName);
+        if (!checkOutMap.containsKey(checkOutKey)) {
+            checkOutMap.put(checkOutKey, new CheckOutInfo(t - checkInInfo.t, 1));
+        } else {
+            checkOutMap.get(checkOutKey).total += t - checkInInfo.t;
+            checkOutMap.get(checkOutKey).count += 1;
         }
-        Stack<Character> s = new Stack<>();
-        StringBuilder postfix = new StringBuilder();
-        for (char c : infix.toCharArray()) {
-            if (c == '(') {
-                s.add(c);
-            } else if (c == ')') {
-                while (s.peek() != '(') {
-                    postfix.append(s.pop());
-                }
-                s.pop();
-            } else if (isOperand(c)) {
-                postfix.append(c);
-            } else {
-                while (!s.isEmpty() && prec(c) >= prec(s.peek())) {
-                    postfix.append(s.pop());
-                }
-                s.add(c);
-            }
-        }
-        while (!s.isEmpty()) {
-            postfix.append(s.pop());
-        }
-        return postfix.toString();
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+        String checkOutKey = String.format("%s-%s", startStation, endStation);
+        CheckOutInfo checkOutInfo = checkOutMap.get(checkOutKey);
+        return (double) checkOutInfo.total / checkOutInfo.count;
     }
 }
